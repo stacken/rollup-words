c84c6d1eded7d961dd84b7a2a68bc1c01a89c83c
Rasmus Kaj <kaj@kth.se>
2016-01-08 23:25:54 +0100

    Add missing requirement.
    (And convert some leftovers to alternative instructions).

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,6 @@
 set -ex
 virtualenv --system-site-packages ve
 . ve/bin/activate
-#git clone git@github.com:amueller/word_cloud.git
-#cd word_cloud
-#pip install -r requirements.txt
-pip install wordcloud
+pip install wordcloud numpy
+# Or git clone git@github.com:amueller/word_cloud.git and follow the
+# installation instructions there.
